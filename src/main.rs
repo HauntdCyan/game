d3fa823c085b53db1614adafca96f670695ef76c
Jessie Semancik <tomsemancik@outlook.cz>
2023-12-29 02:15:35 +0100

    BIG ASS CHANGES THINGS ACTUALLY WINNABLE NOW NOT LOOSABLE THO

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,29 @@ let enemy_name = enemy[enemyrng];
 let mut enemy_hp = enemyhp;
 
 println!("{enemy_name} appears!! {enemyhp}HP {enemyatt}ATT \n",);
-println!("{enemy_name}");
+println!("BOB:");//PLACEHOLDER FOR PLAYER'S NAME
 let hp = 100;//hp will not reset after fights later
 let power = rand::thread_rng().gen_range(15..=20);
 
 loop {//battle loop start
 let ran_away = false;    
 
+
 println!("HP:{hp}");//the battle intro
 println!("ATTACK POWER:{power}\n");
 println!("What will you do?");
-println!("ATTACK SKILL RUN");
+println!("ATTACK SKILL CHECK RUN");
 
 let mut selection = String::new();
 
 io::stdin()
 .read_line(&mut selection)
 .expect("You cannot do that");                                             //selections
+print!("{}[2J", 27 as char);                    //clears terminal
 
 if selection.to_lowercase().contains("attack"){                                 //attacking
 
-let mut new_enemy_hp = enemy_hp - power;
-
-let mut enemy_hp = new_enemy_hp;
+ enemy_hp = enemy_hp - power;
 
 
 println!("\nYou attack {enemy_name} causing {power}damage!!");
@@ -45,7 +45,14 @@ println!("\n{enemy_name} now has {enemy_hp} hp left!\n");
 
 }
 //add attack doing something
-if selection.to_lowercase().contains("run") { let ran_away = running(); }
+if selection.to_lowercase().contains("run") { let ran_away = running(); } //FIX
+
+
+if selection.to_lowercase().contains("check") {//displays enemy's stats
+    println!("\n\n{enemy_name}:");
+    println!("{enemy_hp}HP {enemyatt}ATT\n\n"); continue;
+}
+
 
 if selection.to_lowercase().contains("Skill") {println!("no skills yet :)")}
 
@@ -53,9 +60,7 @@ if selection.to_lowercase().contains("Skill") {println!("no skills yet :)")}
 if ran_away == true {break;}//breaks loop if run succeeded
 
  //else { println! ("You can't do that");} //guhh??
-//println!(" ene test {enemyhp}");// enemy hp test
-//add choice slection and turns for battles with loops
-
+if enemy_hp < 0 {println!("you win!!"); break;} 
 selection.truncate(0);
 }//end of battle loop
 
