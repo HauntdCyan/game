c07a84426d4b62e6b43d7c69b1e4d512902838a9
Jessie Semancik <tomsemancik@outlook.cz>
2024-01-13 03:20:14 +0100

    experimenting

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,28 @@ use rand::Rng; //
 use std::io;
 fn main(){
 
-    let roomchances = rand::thread_rng().gen_range(0..=200);
 
-
+roomgen::roomtype 
     
-let roomtype =["loot", "enemy", "boss", "camp"];
-let room = roomtype [rand::thread_rng().gen_range(0..=4)];
+println!("left:{} Mid:{} Right:{}",leftdoor,middledoor,rightdoor);
+
+}
+
+fn roomgen()
+{
+let directions  Directions::Left;//left middle right doorways
+
+let roomtype =["loot", "enemy", "boss", "empty", "camp"];
+let leftdoor  = roomtype [rand::thread_rng().gen_range(1..=5)];
+let middledoor = roomtype [rand::thread_rng().gen_range(1..=5)];
+let rightdoor = roomtype [rand::thread_rng().gen_range(1..=5)];
 
 
 
-println!("{}",room)
 
-
+}
+enum Directions{
+    left,
+    right,
+    middle,
 }
