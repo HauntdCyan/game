e0ffd4c689cf44030b11f09993da234dda8c7700
Jessie Semancik <tomsemancik@outlook.cz>
2024-01-16 09:40:35 +0100

    room randomization

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,27 @@ fn roomgen()
 let mut leftdoor = false;
 let mut middledoor = false;
 let mut rightdoor = false;
-    let directions = rand::thread_rng().gen_range(1..=5); //left middle right doorways
 
 
 
 let roomtype =["loot", "enemy", "boss", "empty", "camp"];
 
+let mut randomization_turn = 0;
 loop{
-    if directions == 0 {continue;}
-if directions > 1 { leftdoor  = true;break;}
-if directions > 2 { middledoor  = true;break;}
-if directions > 3 { rightdoor  = true;break;}
+
+let directions = rand::thread_rng().gen_range(1..=5); //left middle right doorways
+if directions == 1 || directions == 2 {randomization_turn += 1;}
+if directions == 3 {randomization_turn += 1;println!("left"); leftdoor  = true;}
+if directions == 4 {randomization_turn += 1;println!("mid"); middledoor  = true;}
+if directions == 5 {randomization_turn += 1;println!("right"); rightdoor  = true;}
+
+
+if leftdoor || middledoor || rightdoor {if randomization_turn > 3 {break;}}
+
+
+
 }
-println!("{directions}");
+//randomization_turn.turncate(0);
 
 println!("{} {} {}",leftdoor,middledoor,rightdoor)
 
