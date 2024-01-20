54069db9046e0d320879c387b4aa22442b8f8730
Jessie Semancik <tomsemancik@outlook.cz>
2024-01-20 03:36:28 +0100

    more roomtype randomization

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use rand::Rng; //
+
+
 fn main(){
 
-   let Doorsopen = randomization();
+   let doorsopen = randomization();
 //let doors = Doorsopen {left, right, middle};
-    if Doorsopen.left == true {println!("shit it left")};
-    if Doorsopen.middle == true {println!("shit it middle")};
-    if Doorsopen.right == true {println!("shit it right")};
-    roomtype()
+    if doorsopen.left == true {println!("shit it left")};
+    if doorsopen.middle == true {println!("shit it middle")};
+    if doorsopen.right == true {println!("shit it right")};
+    
 //println!("left:{} Mid:{} Right:{}",leftdoor,middledoor,rightdoor);
 
 }
@@ -17,27 +19,38 @@ let mut leftdoor = false;
 let mut middledoor = false;
 let mut rightdoor = false;
 
+let roomtype = ["loot", "enemy", "boss", "empty", "camp"];
 
 
-let roomtype =["loot", "enemy", "boss", "empty", "camp"];
 
 let mut randomization_turn = 0;
 loop{
 
+let mut number_of_directions = 0;
+
 let directions = rand::thread_rng().gen_range(1..=5); //left middle right doorways
 
 if directions == 1 || directions == 2 {randomization_turn += 1;}
-if directions == 3 {randomization_turn += 1; leftdoor  = true;}
-if directions == 4 {randomization_turn += 1; middledoor  = true;}
-if directions == 5 {randomization_turn += 1; rightdoor  = true;}
+if directions == 3 {number_of_directions += 1; randomization_turn += 1; leftdoor  = true;}
+if directions == 4 {number_of_directions += 1; randomization_turn += 1; middledoor  = true;}
+if directions == 5 {number_of_directions += 1; randomization_turn += 1; rightdoor  = true;}
+
 
 
 if leftdoor || middledoor || rightdoor {if randomization_turn > 3 {
     
+let room = directions * randomization_turn + rand::thread_rng().gen_range(000..=500);
+
+if room [100..=200] {println!("cool!!!")}//fix
+
+println!("{room}");
+
 let doorsopen = Doorsopen{
 left:leftdoor,
 middle:middledoor,
-right:rightdoor};
+right:rightdoor
+
+};
     
     return doorsopen
 }}
